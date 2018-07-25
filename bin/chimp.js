4b4aef42733b24881abf247578fe14b3f78e67e5
Łukasz Gandecki <lgandecki@css.edu>
2018-07-25 17:20:27 +0400

    somehow this coma breakes node v6

--- bin/chimp.js
+++ bin/chimp.js
@@ -4,7 +4,7 @@ const colors = require('colors');
 colors.enabled = true;
 log.info("\n\nThanks for using Chimpy!".green);
 
-log.info("\nPlease change your chimp calls to chimpy.".yellow, "\nUsing the old name - chimp - to run chimpy is deprecated.\nIn the near future it will collide with chimp 2.0 project.\n\n".yellow, );
+log.info("\nPlease change your chimp calls to chimpy.".yellow, "\nUsing the old name - chimp - to run chimpy is deprecated.\nIn the near future it will collide with chimp 2.0 project.\n\n".yellow );
 
 setTimeout(() => {
     require('../dist/bin/chimp');
