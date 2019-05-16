873cea2c0951257dc69e4069c761f5b75274e72b
alanning <alanning@gmail.com>
2019-05-16 15:27:17 -0400

    support import syntax in config file

--- bin/chimpy.js
+++ bin/chimpy.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 
-// be sure to run `npm run prepublish` first to generate the dist dir
+require('@babel/register')
+
+// be sure to run `npm run prepare` first to generate the dist dir
 require('../dist/bin/chimp');
